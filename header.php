699731d1f3403cd489fb6a065d57adefc92832c5
Victor KANNEMACHER <vkannema@e3r12p18.42.fr>
2017-07-04 18:14:14 +0200

    head and foot

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My e-Shop - Rush 42</title>
+
+	<link rel="stylesheet" href="style.css">
+
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,700" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+
+	<!-- Favicon -->
+	<link href="img/42.png" rel="icon" type="image/svg">
+</head>
+<body>
+
+	<nav>
+
+
+
+	</nav>
