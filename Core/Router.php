0e54e2b06e9a4270625f293dfb3e804498df2a00
Victor <vkannema@student.42.fr>
2017-07-13 21:48:16 +0200

    started router

---
+++ Core/Router.php
@@ -0,0 +1,55 @@
+<?php
+
+class Router
+{
+
+	protected $routes= [];
+
+	protected $params = [];
+
+	public function add($route, $params = [])
+	{
+		$route = preg_replace('/\//', '\\/', $route);
+		$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+		$route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+		$route = '/^' . $route . '$/i';
+
+		$this->routes[$route] = $params;
+	}
+
+	public function getRoutes()
+	{
+		return $this->routes;
+	}
+
+	public function match($url)
+	{
+		// foreach ($this->routes as $route => $params)
+		// {
+		// 	if ($url == $route)
+		// 	{
+		// 		$this->params = $params;
+		// 		return true;
+		// 	}
+		// }
+
+		// $reg_exp = "/^(?P<controller>[a-z-]+)\/(?<action>[a-z-]+)$/";
+		foreach ($this->routes as $route => $params) {
+			if (preg_match($route, $url, $matches)) {
+				foreach ($matches as $key => $match) {
+					if (is_string($key)) {
+						$params[$key] = $match;
+					}
+				}
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public function getParams()
+	{
+		return $this->params;
+	}
+}
--- public/index.php
+++ public/index.php
@@ -1,7 +1,37 @@
 <?php
 
-require_once("../app/init.php");
+require('../Core/Router.php');
 
-$app = new App;
+$router = new Router();
+
+// echo get_class($router);
+
+//Add the routes
+
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+//$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// '</pre>';
+
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url))
+{
+	echo '<pre>';
+	var_dump($router->getParams());
+	echo '</pre>';
+}
+else {
+	echo "No route found for URL '$url'";
+}
 
 ?>
